1eacf07079588ed178335f54e3b99a9b87e8c453
Eric <kelsric@yahoo.com>
2022-06-26 20:15:34 +0300

    editted style for html

--- myproject/index.js
+++ myproject/index.js
@@ -1,14 +1,4 @@
-const options = {
-    method: 'GET',
-    url: 'https://hotels4.p.rapidapi.com/locations/v2/search',
-    params: {query: 'new york', locale: 'en_US', currency: 'USD'},
-    headers: {
-      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-      'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-    }
-  };
-  
-  const FIXER_API_KEY = "YOUR_FIXER_KEY_HERE";
+const FIXER_API_KEY = "YOUR_FIXER_KEY_HERE";
 
 let symbols = [];
 
